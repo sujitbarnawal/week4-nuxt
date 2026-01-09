1ff6f7812bbe342e7cce8507609a6867b1c13fc5
sujitbarnawal <sujitbarnawal4545@gmail.com>
2026-01-09 08:31:02 +0000

    feat: Added Footer Section

--- day1-application/app/components/Footer.vue
+++ day1-application/app/components/Footer.vue
@@ -1,3 +1,30 @@
 <template>
-    footer
+    <footer class="footer">
+        <p>
+            © {{new Date().getFullYear()}} Sujit<span>Barnawal</span> 
+        </p>
+
+    </footer>
 </template>
+
+<style scoped>
+    .footer{
+        position: fixed;
+        bottom: 0;
+        height: 60px;
+        background-color: white;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        justify-content: center;
+    }
+    p{
+        font-size: 20px;
+        font-weight: bolder;
+    }
+
+    span{
+        color: crimson;
+    }
+
+</style>
