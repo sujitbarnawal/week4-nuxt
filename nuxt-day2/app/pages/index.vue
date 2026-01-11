19a1bf7ca54b3ba9ef6ec814a78d1a111823b00e
sujitbarnawal <sujitbarnawal4545@gmail.com>
2026-01-11 15:42:07 +0000

    fix(routing): single root node problem fixed

--- nuxt-day2/app/pages/index.vue
+++ nuxt-day2/app/pages/index.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
 import type { Post } from '~/types/PostType';
-
-    // import { useCounter } from '../composables/useCounter';
-    // const {count,increment}=useCounter()
     const {data: posts}=await useFetch<Post[]>("https://jsonplaceholder.typicode.com/posts")
 </script>
 
 <template>
-    <!-- <h1>Counter</h1>
-    <p>{{ count }}</p>
-    <button @click="increment">+</button> -->
     <div class="blog-list">
         <h1>Blog</h1>
         <NuxtLink class="block" v-for="post in posts" :key="post.id" :to="`/posts/${post.id}`">
