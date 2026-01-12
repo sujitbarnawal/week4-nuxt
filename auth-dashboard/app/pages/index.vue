9b302c0cfd19694c93fe41b5c5f68a22a672e025
sujitbarnawal <sujitbarnawal4545@gmail.com>
2026-01-12 06:32:26 +0000

    fix: email validation fixed by using form

--- auth-dashboard/app/pages/index.vue
+++ auth-dashboard/app/pages/index.vue
@@ -6,7 +6,7 @@ const loading = ref(false)
 const name = ref("")
 const email = ref("")
 
-const {  login } = useAuth()
+const { login } = useAuth()
 
 const submit = async () => {
     error.value = ""
@@ -34,7 +34,7 @@ const submit = async () => {
 
 <template>
     <div class="min-h-screen flex items-center justify-center">
-        <div class="w-full  max-w-md bg-white rounded-xl shadow-lg p-8">
+        <form @submit.prevent="submit" class="w-full  max-w-md bg-white rounded-xl shadow-lg p-8">
             <h1 class="text-2xl font-semibold text-center mb-6">Login</h1>
             <div class="text-red-500 text-sm mb-4" v-if="error">{{ error }}</div>
 
@@ -44,12 +44,12 @@ const submit = async () => {
             <input type="email" placeholder="Enter Your Email" v-model="email"
                 class="w-full mb-4 px-4 py-2 border rounded-lg focus:outline-none focus:ring">
 
-            <button @click="submit" class="w-full bg-blue-600 hover:bg-blue-700 transition disabled:opacity-50 disabled:cursor-not-allowed
+            <button type="submit" class="w-full bg-blue-600 hover:bg-blue-700 transition disabled:opacity-50 disabled:cursor-not-allowed
  text-white px-4 py-2 rounded-lg text-xl" :disabled="loading">
                 {{ loading ? "Logging in..." : "Login" }}
             </button>
 
-        </div>
+        </form>
 
     </div>
 </template>
